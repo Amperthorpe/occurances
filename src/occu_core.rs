bed5d3b76321025dfa3cd6d1dc0b6568d45f3667
Luna <luna@lunaskye.net>
2025-06-04 18:15:20 -0700

    fix str borrow issue

--- src/occu_core.rs
+++ src/occu_core.rs
@@ -63,31 +63,22 @@ impl Event {
 
 impl fmt::Display for Event {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let uuid_string = self.uuid.to_string();
-        let u_string_len = uuid_string.len();
-        let uuid_end = uuid_string
-            .char_indices()
-            .filter(|charidx| {
-                let (idx, cha) = charidx;
-                let asd = &u_string_len;
-                if idx > asd {}
-            })
-            .collect();
+        let last_four = last_four_chars(self.uuid.to_string());
         write!(
             f,
             "Event ( uuid: {}, title: {}, occurances: WIP )",
-            self.uuid, self.title
+            last_four, self.title
         )
     }
 }
 
 // Misc
 
-fn last_four_chars(s: &str) -> &str {
+fn last_four_chars(s: String) -> String {
     let char_count = s.chars().count();
     if char_count <= 4 {
         return s; // Return entire string if <= 4 chars
     }
     let start = s.char_indices().nth(char_count - 4).unwrap().0;
-    &s[start..]
+    s[start..].to_string()
 }
